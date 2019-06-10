19f4f77848607fe6039e35c153684d092c131be7
Cezarig <51354067+Cezarig@users.noreply.github.com>
2019-06-10 12:49:47 +0300

    Add files via upload

---
+++ Variable type in C sharppppppppppp/Variable type in C sharppppppppppp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variable_type_in_C_sharppppppppppp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //bool b = true;
+
+            //b = 1;  Error of NOOOOBS !!!
+
+            //Console.WriteLine("Min = {0}", int.MinValue);
+            //Console.WriteLine("Max = {0}", int.MaxValue);
+
+            string Name = @"\nBoule";    // @ e smeker - verbatim literal
+            Console.WriteLine(Name);
+            string path = @"C:\Users\cezar";
+        }
+    }
+}
