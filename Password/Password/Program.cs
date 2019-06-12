4b2613282262b9c6fa033eee3a5f85e9ad13e080
Cezarig <51354067+Cezarig@users.noreply.github.com>
2019-06-12 15:02:37 +0300

    Add files via upload

---
+++ Password/Password/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CryptSharp;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CryptedPassword = Crypter.MD5.Crypt("Bcrypt"), TryPass;
+            Console.WriteLine("Type your password :");
+
+            TryPass = Console.ReadLine();
+
+            if(Crypter.CheckPassword(TryPass, CryptedPassword))
+            {
+                Console.Write("Unlocked");
+            }
+            else
+            {
+                Console.Write("Locked");
+            }
+
+        }
+    }
+}
